cc03aa93d52f98a35851622937b1addd6a880241
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2022-03-24 14:57:33 +0400

    tesst

--- app/src/main/java/com/example/mquizapp/Constants.kt
+++ app/src/main/java/com/example/mquizapp/Constants.kt
@@ -5,61 +5,61 @@ object Constants {
         val questionList = ArrayList<Question>()
 
         val que1 = Question(
-            1, "what country does this flag belong to",
+            1, "What country does this flag belong to?",
             R.drawable.ic_flag_of_arg,
             "Argentina", "Australia", "Brazil", "India",
           1
         )
 
         val que2 = Question(
-            2, "what country does this flag belong to",
+            2, "What country does this flag belong to?",
             R.drawable.ic_flag_of_br,
             "Brazil", "Australia", "Georgia", "Uk",
             1
         )
 
         val que3 = Question(
-            3, "what country does this flag belong to",
+            3, "What country does this flag belong to?",
             R.drawable.ic_flag_of_ger,
             "German", "Uk", "France", "US",
             1
         )
         val que4 = Question(
-            4, "what country does this flag belong to",
+            4, "What country does this flag belong to?",
             R.drawable.ic_flag_of_geo,
             "Georgia", "Australia", "None", "India",
             1
         )
 
         val que5 = Question(
-            5, "what country does this flag belong to",
+            5, "What country does this flag belong to?",
             R.drawable.ic_flag_of_in,
             "India", "Australia", "Georgia", "Uk",
             1
         )
 
         val que6 = Question(
-            6, "what country does this flag belong to",
+            6, "What country does this flag belong to?",
             R.drawable.ic_flag_of_it,
             "Italy", "German", "France", "US",
             1
         )
         val que7 = Question(
-            7, "what country does this flag belong to",
+            7, "What country does this flag belong to?",
             R.drawable.ic_flag_of_tur,
             "Turky", "Australia", "Brazil", "India",
             1
         )
 
         val que8 = Question(
-            8, "what country does this flag belong to",
+            8, "What country does this flag belong to?",
             R.drawable.ic_flag_of_uk,
-            "Ukrain", "Australia", "Georgia", "Uk",
+            "Uk", "Australia", "Georgia", "Ukrain",
             1
         )
 
         val que9 = Question(
-            9, "what country does this flag belong to",
+            9, "What country does this flag belong to?",
             R.drawable.ic_flag_of_fr,
             "France", "German", "France", "US",
             1
--- app/src/main/java/com/example/mquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/mquizapp/QuizQuestionsActivity.kt
@@ -25,9 +25,38 @@ class QuizQuestionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion =  findViewById(R.id.tv_quastion)
+        ivImage = findViewById(R.id.iv_image)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptiontwo = findViewById(R.id.tv_option_two)
+        tvOptionthree = findViewById(R.id.tv_option_three)
+        tvOptionfour = findViewById(R.id.tv_option_four)
 
 
         val questionList = Constants.getQuestions()
-        Log.i("Questionslist size is, ${questionList.size}")
+        Log.i("Questionslist size is", "${questionList.size}")
+
+        for (i in questionList){
+            Log.e("Questions", i.question)
+        }
+
+
+        val currentPosition = 1
+        val question : Question = questionList [currentPosition - 1]
+        ivImage?.setImageResource(question.image)
+        progressBar?.progress = currentPosition
+        tvProgress?.text = "$currentPosition/${progressBar?.max}"
+        tvQuestion?.text = question.question
+        tvOptionOne?.text = question.opotionOne
+        tvOptiontwo?.text = question.opotiontwo
+        tvOptionthree?.text = question.opotionthree
+        tvOptionfour?.text = question.opotionfour
+
+
+
     }
 }
+
+//106 18:30
